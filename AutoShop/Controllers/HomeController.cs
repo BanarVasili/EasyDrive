152c4080385af38b8d2e7254c98679acd85d15af
Gamerprice <Gamerprice@gmail.com>
2024-02-19 17:31:01 +0200

    Update HomeController

--- AutoShop/Controllers/HomeController.cs
+++ AutoShop/Controllers/HomeController.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace AutoShop.Controllers
@@ -22,6 +19,21 @@ namespace AutoShop.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
+            return View();
+        }
+
+        public ActionResult Services()
+        {
+            return View();
+        }
+
+        public ActionResult Gallery()
+        {
+            return View();
+        }
+
+        public ActionResult Client()
+        {
             return View();
         }
     }
