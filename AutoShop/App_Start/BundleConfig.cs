1ecdee00a26bcef5439d65e3a5b85b8309f4fbd8
BanarVasili <BanarVasili@gmail.com>
2024-02-19 15:18:40 +0200

    Add bundle config

--- AutoShop/App_Start/BundleConfig.cs
+++ AutoShop/App_Start/BundleConfig.cs
@@ -1,30 +1,46 @@
 ﻿using System.Web;
 using System.Web.Optimization;
 
-namespace AutoShop
+public class BundleConfig
 {
-    public class BundleConfig
+    // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+    public static void RegisterBundles(BundleCollection bundles)
     {
-        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
-        public static void RegisterBundles(BundleCollection bundles)
-        {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+        bundles.Add(new StyleBundle("~/Content/styles").Include(
+            "~/Content/css/bootstrap.min.css",
+            "~/Content/css/style.css",
+            "~/Content/css/responsive.css",
+            "~/Content/css/jquery.mCustomScrollbar.css"
+            ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                "~/Scripts/jquery.validate*"));
+        // JavaScript bundle
+        bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+            "~/Scripts/jquery-{version}.min.js"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                "~/Scripts/modernizr-*"));
+        bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+            "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                "~/Scripts/bootstrap.js"));
+        // Use the development version of Modernizr to develop with and learn from. Then, when you're
+        // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+        bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+            "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                "~/Content/bootstrap.css",
-                "~/Content/site.css"));
-        }
+        bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            "~/Scripts/bootstrap.js",
+            "~/Scripts/bootstrap.bundle.js"));
+
+        // Custom JavaScript files
+        bundles.Add(new ScriptBundle("~/bundles/custom").Include(
+            "~/Scripts/custom.js",
+            "~/Scripts/jquery.mCustomScrollbar.concat.min.js",
+            "~/Scripts/popper.min.js",
+            "~/Scripts/plugin.js",
+            "~/Scripts/slider-setting.js"));
+
+        // Code for the rest of your bundles goes here
+
+        // Set EnableOptimizations to false for debugging. For more information,
+        // visit http://go.microsoft.com/fwlink/?LinkId=301862
+        BundleTable.EnableOptimizations = true;
     }
 }
