72c4b9d0100af8dfe461a7306bb561f3a4df3651
Slavick03 <Slavick03@gmail.com>
2024-03-08 16:30:52 +0300

    Add migrations

---
+++ Autoshop.Infrastructure/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity.Migrations;
+
+namespace Autoshop.Infrastructure.Migrations
+{
+    internal sealed class Configuration : DbMigrationsConfiguration<Autoshop.Infrastructure.JewelryStoreContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Autoshop.Infrastructure.JewelryStoreContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
