158e7cf87bd45ae11917d5a9bb28b93be56dbf11
Slavick03 <Slavick03@gmail.com>
2024-03-20 15:36:48 +0300

    implement admin controller

---
+++ AutoShop.WebUI/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace AutoShop.WebUI.Controllers
+{
+    [Authorize(Roles = "Moderator")]
+    public class AdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
