9d4212b32fe7ceb0d6ff3e0692b40eddcb4623aa
Gamerprice <Gamerprice@gmail.com>
2024-03-28 23:34:46 +0300

    change post auth logic

--- AutoShop.WebUI/Global.asax.cs
+++ AutoShop.WebUI/Global.asax.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
+using Autofac;
+using Autofac.Integration.Mvc;
+using Autoshop.Infrastructure;
 
 namespace AutoShop.WebUI
 {
@@ -12,10 +18,40 @@ namespace AutoShop.WebUI
     {
         protected void Application_Start()
         {
+            // Стандартная настройка MVC
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Настройка Autofac
+            var builder = new ContainerBuilder();
+
+            // Регистрация DbContext. Предполагается, что MyStoreContext - ваш класс контекста
+            builder.RegisterType<JewelryStoreContext>().AsSelf().InstancePerRequest();
+
+            // Остальные регистрации зависимостей
+            // builder.RegisterType<SomeDependency>().As<ISomeInterface>();
+
+            // Создаем новый контейнер с зарегистрированными зависимостями
+            var container = builder.Build();
+
+            // Заменяем стандартный DependencyResolver на AutofacDependencyResolver
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+        
+        protected void Application_PostAuthenticateRequest(Object sender, EventArgs e)
+        {
+            HttpCookie authCookie = Context.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie != null)
+            {
+                FormsAuthenticationTicket authTicket = FormsAuthentication.Decrypt(authCookie.Value);
+                if (authTicket != null && !authTicket.Expired)
+                {
+                    var roles = authTicket.UserData.Split(',');
+                    HttpContext.Current.User = new GenericPrincipal(new FormsIdentity(authTicket), roles);
+                }
+            }
         }
     }
 }
